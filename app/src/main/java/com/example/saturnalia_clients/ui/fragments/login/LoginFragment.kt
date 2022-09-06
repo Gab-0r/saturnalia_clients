e49849d9159e974b31d9e17286a4ee1fd9c555be
Gabriel <jgabriel.orozco@udea.edu.co>
2022-09-06 16:09:55 -0500

    Se realiza la verificación de inicio de sesión satisfactoriamente.

--- app/src/main/java/com/example/saturnalia_clients/ui/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/example/saturnalia_clients/ui/fragments/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.example.saturnalia_clients.databinding.FragmentLoginBinding
@@ -22,21 +23,35 @@ class LoginFragment : Fragment() {
         loginBinding = FragmentLoginBinding.inflate(inflater, container, false)
         loginviewModel = ViewModelProvider(this)[LoginViewModel::class.java]
 
+        loginviewModel.erroMsg.observe(viewLifecycleOwner){
+            showErrorMsg(it)
+        }
+
+        loginviewModel.loginSuccess.observe(viewLifecycleOwner){
+            goToHome()
+        }
+
         with(loginBinding){
-            signUpButton.setOnClickListener {
-                findNavController().navigate(LoginFragmentDirections.actionNavigationLoginToNavigationSignUp())
-            }
             loginButton.setOnClickListener {
-                //findNavController().navigate(LoginFragmentDirections.actionNavigationLoginToNavigationProfile())
+                loginviewModel.checkFields(emailInputLayout.text.toString(), passwordEditTextTextPassword.text.toString())
+            }
+            loginBinding.signUpButton.setOnClickListener {
+                findNavController().navigate(LoginFragmentDirections.actionNavigationLoginToNavigationSignUp())
             }
         }
 
         return loginBinding.root
     }
 
+    fun goToHome(){
+        findNavController().navigate(LoginFragmentDirections.actionNavigationLoginToNavigationProfile())
+    }
+
     override fun onResume() {
         super.onResume()
         (activity as AppCompatActivity).supportActionBar!!.hide()
     }
-
+    private fun showErrorMsg(msg_: String?) {
+        Toast.makeText(requireActivity(), msg_, Toast.LENGTH_LONG).show()
+    }
 }
--- app/src/main/java/com/example/saturnalia_clients/ui/fragments/login/LoginViewModel.kt
+++ app/src/main/java/com/example/saturnalia_clients/ui/fragments/login/LoginViewModel.kt
@@ -1,6 +1,43 @@
 package com.example.saturnalia_clients.ui.fragments.login
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.saturnalia_clients.ui.data.ResourceRemote
+import com.example.saturnalia_clients.ui.data.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
+
+    private val userRepository = UserRepository()///Repositorio (Fuente de datos  para la aplicación)
+
+    private val _errorMsg: MutableLiveData<String?> = MutableLiveData()//Mensaje de error
+    val erroMsg: LiveData<String?> = _errorMsg //Mensaje de error que se le pasa a la vista
+
+    private val _loginSuccess: MutableLiveData<String?> = MutableLiveData()
+    val loginSuccess: LiveData<String?> = _loginSuccess
+
+    fun checkFields(email_: String, pass_: String) {
+        if(email_.isEmpty() || pass_.isEmpty())
+            _errorMsg.value = "Todos los campos deben ser llenados"
+        if(pass_.length < 6)
+            _errorMsg.value = "La contraseña debe tener como mínimo 6 caracteres"
+        else
+            GlobalScope.launch(Dispatchers.IO){
+                val result = userRepository.loginUser(email_, pass_)
+                result.let{resourceRemote ->
+                    when(resourceRemote){
+                        is ResourceRemote.success ->{
+                            _loginSuccess.postValue(result.data)
+                            _errorMsg.postValue("Bienvenido")
+                        }
+                        is ResourceRemote.error -> {
+                            var msg = result.message
+
+                        }
+                    }
+                }
+
+            }
+    }
 }
