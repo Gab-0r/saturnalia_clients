750829078fd6ad799374ae8c70ef7b4badf15c8e
Gabriel <jgabriel.orozco@udea.edu.co>
2022-09-28 06:16:51 -0500

    Registro y edición del perfil corregidos.

--- app/src/main/java/com/example/saturnalia_clients/ui/model/Disco.kt
+++ app/src/main/java/com/example/saturnalia_clients/ui/model/Disco.kt
@@ -6,7 +6,7 @@ data class Disco(
     var uid: String? = null,
     var name: String? = null,
     var email: String? = null,
-    var rating: Float? = 0f,
+    var rating: Float? = null,
     var about: String? = null,
     var address: String? = null,
     var phone: String? = null
