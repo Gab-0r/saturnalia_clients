066a02fa9b505e28de05520ccf12a91f3f21cfa3
Gabriel <jgabriel.orozco@udea.edu.co>
2022-09-28 06:26:44 -0500

    Vista de ajustes implementada

--- app/src/main/java/com/example/saturnalia_clients/ui/fragments/config/ConfigFragment.kt
+++ app/src/main/java/com/example/saturnalia_clients/ui/fragments/config/ConfigFragment.kt
@@ -7,26 +7,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.saturnalia_clients.R
+import com.example.saturnalia_clients.databinding.FragmentConfigBinding
 
 class ConfigFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ConfigFragment()
-    }
-
-    private lateinit var viewModel: ConfigViewModel
+    private lateinit var configBinding: FragmentConfigBinding
+    private lateinit var configViewModel: ConfigViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_config, container, false)
-    }
+    ): View {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ConfigViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        configBinding = FragmentConfigBinding.inflate(inflater, container, false)
+        configViewModel = ViewModelProvider(this)[ConfigViewModel::class.java]
 
+        val view = configBinding.root
+
+        return view
+    }
 }
